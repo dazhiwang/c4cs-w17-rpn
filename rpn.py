9b0809098237c55ed508cf4f8d9cf9c4aab59e95
Dazhi Wang <dazhi@umich.edu>
2017-03-10 15:00:24 -0500

    lalala

---
+++ rpn.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+
+def calculate(arg):
+        pass
+
+def main():
+        while True:
+                calculate(input('rpn calc>'))
+
+# RUN as main or as module
+if __name__ == '__main__':
+        main()
+
